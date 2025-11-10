3721aab8b45de76da178153cb3beaab6a0440751
souvik mndal <souvkmndal@gmail.com>
2025-11-10 21:19:25 +0530

    rest parameter to add unlimited numbers

--- 03.JS/30.rest parameter to add unlimited numbers.js
+++ 03.JS/30.rest parameter to add unlimited numbers.js
@@ -1,8 +1,14 @@
+// function add(...rest){
+//     sum = 0;
+//     rest.forEach((x)=>{
+//         sum+=x
+//     })
+//     console.log(`The sum is : ${sum}`);
+// }
 function add(...rest){
-    sum = 0;
-    rest.forEach((x)=>{
-        sum+=x
-    })
-    console.log(`The sum is : ${sum}`);
+    let ans = rest.reduce((acc,cur)=>{
+        return acc += cur;
+    },0)
+    console.log(`The sum is : ${ans}`);
 }
 add(1,2,3,4,5,6,7,8,9,10)
