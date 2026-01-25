79d5ee3447f3236e197ad3e891c90139580871c7
souvik mndal <souvkmndal@gmail.com>
2026-01-25 19:12:08 +0530

    promise hell

---
+++ 03.JS/08.promise hell/script.js
@@ -0,0 +1,44 @@
+function searchFood(food){
+    let promise = new Promise((res,rej)=>{
+        console.log(`Searching started for food ${food}`);
+        setTimeout(() => {
+            let data = `list of ${food}`
+            res([data,food])
+        }, 3000);
+    })
+    return promise
+}
+
+
+
+function orderFood(food){
+    console.log(`${food} selected`);
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            let id = 213456;
+            res(id)
+        }, 3000);
+    })
+}
+
+function payment(food ,id ){
+    console.log(`payment started for ${food} with id ${id}`);
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            let r = true;
+            res(`payment ended successfully with response ${r}`)
+        }, 4000);
+    })
+}
+
+
+searchFood("Biriyani").then((item)=>{
+    console.log(item[0]);
+    orderFood(item[1]).then((id)=>{
+        payment(item[1],id).then((r)=>{
+            console.log(r);
+        })
+    })
+})
+
+
